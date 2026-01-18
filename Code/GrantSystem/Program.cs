f0c18f123a0d78a8653da1a75e6151c1931dfeed
TankistPro <nakol.nayk@mail.ru>
2026-01-18 16:04:08 +0300

    fix

---
+++ Code/GrantSystem/Program.cs
@@ -0,0 +1,14 @@
+﻿using GrantSystem.Interfaces;
+using GrantSysytem.Domain;
+
+namespace GrantSystem.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IUserRepository<BaseUser> userRepository = new UserRepositry();
+
+        }
+    }
+}
